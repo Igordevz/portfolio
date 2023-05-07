f64cc9c1d7fe97ce74e6a6d8231bb95adc8a681f
Igordevz <programadorigor.dev@gmail.com>
2023-05-06 23:01:15 -0300

    commit

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -1,16 +1,16 @@
-import { ContentMain } from './style';
+import { ContentMain } from "./style";
 
-export default function Main(){
-
-
-    return(
-        <div>
-            <ContentMain>
-                <div className="center">
-                    <h1>I’m a Programmer specialising in <strong>Design</strong> and <strong>Developer FullStack</strong></h1>
-                </div>
-          
-            </ContentMain>
+export default function Main() {
+  return (
+    <div>
+      <ContentMain>
+        <div className="center">
+          <h1>
+            I’m a Programmer specialising in <strong>Design</strong> and{" "}
+            <strong>Developer FullStack</strong>
+          </h1>
         </div>
-    );
+      </ContentMain>
+    </div>
+  );
 }
