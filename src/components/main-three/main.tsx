1415ad30393a5a548d6846ea329a8c74208ee7f2
Igordevz <programadorigor.dev@gmail.com>
2023-05-10 21:40:49 -0300

    animation 10/05 finish

--- src/components/main-three/main.tsx
+++ src/components/main-three/main.tsx
@@ -1,51 +1,131 @@
-import { Container } from "./style";
-import { useEffect } from 'react'
-// import seta from "../../assets/ceta.png";
+import { Container, ContainerThree, ContainerTour, MainContent, NavMenu } from "./style";
+import { useEffect, useState } from 'react'
 import { motion, useTransform, useViewportScroll } from "framer-motion";
 import gsap from 'gsap'
+import close from '../../assets/x.png'
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+import menu from '../../assets/menu.png'
+import SplitType from "split-type";
 
 export default function SemiFooter() {
   const { scrollYProgress } = useViewportScroll()
-  const scale = useTransform(scrollYProgress, [0.90, 1], [0, 1]);
-  useEffect(() =>{
-  gsap.registerPlugin(ScrollTrigger)
-  
+  const [menus, setMenus] = useState(true)
+  const scale = useTransform(scrollYProgress, [0.60, 0.70], [0, 1]);
+  const scroll = useTransform(scrollYProgress, [0, 1], [0, 300]);
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger)
+
     gsap.to("#Container", {
-        scrollTrigger: {
-          trigger: "#Container",
-          start: "top center",
-          end:"bottom center",
-          toggleActions: "restart pause reverse"
-        },
-        display: "flex",
-        x: 0,
-        opacity: 1,
-        duration: 0.1,
+      scrollTrigger: {
+        trigger: "#Container",
+        start: "top center",
+        end: "bottom center",
+        toggleActions: "restart pause reverse"
+      },
+      display: "flex",
+      x: 0,
+      opacity: 1,
+      duration: 0.1,
     })
     gsap.to('.animator1', {
       scrollTrigger: {
         trigger: ".animator1",
         start: "top center",
-        end:"bottom center",
+        end: "bottom center",
         toggleActions: "restart pause reverse"
       },
       display: "flex",
     })
+    const menusBtn = document.getElementById("menus")
+    const close = document.getElementById("close")
+
+    menusBtn?.addEventListener("click", () => {
+
+      gsap.to(".navmenu", {
+        x: 0,
+        display: "flex",
+        duration: 1,
+
+      })
+    })
+    close?.addEventListener("click", () => {
+      gsap.to(".navmenu", {
+        x: 2000,
+        display: "none",
+        duration: 1,
+      })
+    })
+    new SplitType("#brand");
+    new SplitType("#textinfo")
+    gsap.to(".char", {
+      scrollTrigger: {
+        trigger: ".animator1",
+        start: "top center",
+        end: "bottom center",
+        toggleActions: "restart pause reverse"
+      },
+      y: 0,
+      stagger: 0.05,
+      delay: 0.1,
+      duration: 1
+    })
+
   })
+
 
   return (
     <>
       <Container style={{ color: "white" }} className=".bg" >
-   
+
         <div id="Container">
         </div>
         <motion.div style={{
-           background: "#282A3A",
-            opacity: scale,
-            width: "100%",
-          }} className="animator1">
+          background: "#F0EFF1",
+          opacity: scale,
+          width: "100%",
 
+        }} className="animator1">
+
+          <ContainerTour color="black" className="tour">
+            <header>
+              <div className="title">Igor</div>
+              <nav>
+                <h1>Let's work together</h1>
+                {menus ? (
+                  <img src={menu} alt="" width={38} id="menus" onClick={() => setMenus(!menus)} />
+                ) : (
+                  <img src={close} alt="" width={23} id="close" onClick={() => setMenus(!menus)} />
+                )}
+              </nav>
+            </header>
+            <NavMenu className="navmenu">
+              <h1>1ola</h1>
+            </NavMenu>
+
+            <MainContent>
+              <div className="scroll">
+                <motion.div style={{ height: scroll, width: 3, background: "#0080FF" }}>
+
+                </motion.div>
+              </div>
+              <div className="title">
+                <h1 id="brand">Brazil</h1>
+                <p id="textinfo">I'm from Brazil, I specialized in Front-End and today I'm fullstack, I also develop in mobiles</p>
+              </div>
+              <div className="swiperx">
+                <h2>experience</h2>
+                <h2>experience</h2>
+                <h2>experience</h2>
+              </div>
+            </MainContent>
+          </ContainerTour>
+             
         </motion.div>
       </Container>
 
--- src/components/main-three/style.ts
+++ src/components/main-three/style.ts
@@ -6,7 +6,7 @@ export const Container = styled.div`
   width: 100%;
   display: flex;
   position: relative;
-  overflow: hidden;
+  overflow-x: hidden;
 
   #Container {
     width: 100%;
@@ -32,7 +32,6 @@ export const Container = styled.div`
       z-index: 999;
       width: 100%;
       background: #222222;
-
     }
     50% {
       z-index: 999;
@@ -45,7 +44,7 @@ export const Container = styled.div`
       z-index: 999;
     }
     60% {
-        background: #000000;
+      background: #000000;
       transform: translateX(900px);
     }
     70% {
@@ -77,4 +76,126 @@ export const Container = styled.div`
       opacity: 1;
     }
   }
+`;
+export const ContainerTour = styled.div`
+  position: relative;
+  header .title {
+    position: absolute;
+    left: 40px;
+    margin-top: -10px;
+    font-size: 30px;
+  }
+  header {
+
+  z-index: 9;
+    position: fixed;
+    top: 0;
+    color: black;
+    align-items: center;
+    display: flex;
+    height: 80px;
+    width: 100%;
+  }
+  header nav {
+    position: absolute;
+    display: flex;
+    right: 90px;
+    gap: 50px;
+    cursor: pointer;
+    h1 {
+      font-size: 15px;
+      position: relative;
+    }
+    h1::after {
+      content: "";
+      width: 100%;
+      height: 2px;
+      background: black;
+      position: absolute;
+      left: 0;
+      bottom: 5px;
+    }
+    img {
+      z-index: 99;
+    }
+  }
+`;
+export const NavMenu = styled.div`
+  position: fixed;
+  display: none;
+  top: 0;
+  right: 0;
+  height: 100vh;
+  width: 40%;
+  background: black;
+  transform: translateX(2000px);
+`;
+export const MainContent = styled.div`
+  height: 80vh;
+  margin-top: 100px;
+  .title {
+    gap: 50px;
+    align-items: center;
+    display: flex;
+    color: black;
+    flex-direction: row;
+  }
+  .title .char {
+    transform: translateY(115px);
+    transition: transform 0.5s;
+  }
+  .scroll {
+    position: fixed;
+    top: 100px;
+    right: 50px;
+    width: 3px;
+    background: white;
+    height: 300px;
+  }
+  .title h1 {
+    -webkit-clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%);
+    clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%);
+    font-weight: 400;
+    font-family: "Poppins", sans-serif;
+    color: black;
+    margin-left: 40px;
+    font-size: 250px;
+  }
+  .title p {
+    font-size: 18px;
+    -webkit-clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%);
+    clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%);
+    font-family: "Poppins", sans-serif;
+    max-width: 350px;
+    font-weight: 400;
+  }
+  .swiperx {
+    display: flex;
+    animation: girator 11s infinite ease-out;
+    flex-direction: row;
+    color: black;
+    gap: 350px;
+  }
+  .swiperx h2 {
+    font-size: 150px;
+  }
+  @keyframes girator {
+    0% {
+      transform: translateX(-100px);
+    }
+
+    50% {
+      transform: translateX(-2000px);
+    }
+    100% {
+      transform: translateX(0px);
+    }
+  }
+`;
+
+export const ContainerThree = styled.div`
+  height: 100vh;
+  width: 100%;
+  background: red;
+  z-index: 9900009;
 `;
